4c37c4de34c51303b61d975c18499936eb25ecf0
Sebastià Sansó <ganstersb2@gmail.com>
2018-05-01 16:40:03 +0200

    first commit

--- Alumne.java
+++ Alumne.java
@@ -6,5 +6,14 @@ public class Alumne {
     private String poblacio;
     private String curs;
     private String grup;
+
+    public Alumne(String nom, String dni, int edat, String poblacio, String curs, String grup) {
+        this.nom = nom;
+        this.dni = dni;
+        this.edat = edat;
+        this.poblacio = poblacio;
+        this.curs = curs;
+        this.grup = grup;
+    }
     
 }
--- PersonalAdministracio.java
+++ PersonalAdministracio.java
@@ -1,19 +1,19 @@
-public class Professor {
+public class PersonalAdministració {
 
     private String nom;
     private String dni;
     private int edat;
     private String poblacio;
     private long sou;
-    private String departament;
+    private String carreg;
     
-    public Professor(String nom, String dni, int edat, String poblacio, long sou, String departament) {
+    public PersonalAdministració(String nom, String dni, int edat, String poblacio, long sou, String carreg) {
         this.nom = nom;
         this.dni = dni;
         this.edat = edat;
         this.poblacio = poblacio;
         this.sou = sou;
-        this.departament = departament;
+        this.carreg = carreg;
     }
     
 }
