dad89e8ce150d936a0074178425dda267fafc5b8
Sebastià Sansó <ganstersb2@gmail.com>
2018-05-01 16:33:27 +0200

    first commit

---
+++ Professor.java
@@ -0,0 +1,10 @@
+public class Alumne {
+
+    private String nom;
+    private String dni;
+    private int edat;
+    private String poblacio;
+    private long sou;
+    private String departament;
+    
+}
